03d4de301bffbed0610601c097d39b433b91124a
Emanuel F. Oliveira <efiuza@avenuecode.com>
2016-07-31 00:57:53 -0300

    app: cpuinfo now prints vendor string...

--- x86/src/app/cpuinfo.s
+++ x86/src/app/cpuinfo.s
@@ -8,8 +8,10 @@
 
     .data
 
+l_string_cpuidsupported:
+    .string "This CPU supports the CPUID instruction!"
 l_string_cpuidnotsupported:
-    .string "This CPU does not support CPUID instruction...\n"
+    .string "This CPU does not support CPUID instruction..."
 
     .text
 
@@ -36,14 +38,56 @@ cpuid_supported:
     ret
 
 main:
+
     pushl %ebp
     movl %esp, %ebp
-    subl $128, %esp          # reserve 16 dwords
+    subl $8, %esp # 2 dwords
+    # -4(%ebp) = buffer address
+    # -8(%ebp) = buffer limit
+    # ~ buffer
+    movl %esp, -8(%ebp)
+    subl $256, %esp
+    movl %esp, -4(%ebp)
+
+    # save %ebx
+    pushl %ebx
 
     call cpuid_supported     # %eax has the result
     cmpl $0, %eax
     jnz 1f
+
+    # print error message and exit...
+    pushl $l_string_cpuidnotsupported
+    call um_puts
+    addl $4, %esp
+    movl $1, %eax # status code 1
+    jmp 4f
+
   1:
+    pushl $l_string_cpuidsupported
+    call um_puts
+    addl $4, %esp
+
+    # get vendor string...
+    movl $0, %eax
+    cpuid
+    movl -4(%ebp), %eax
+    movl %ebx, (%eax)
+    movl %edx, 4(%eax)
+    movl %ecx, 8(%eax)
+    movl $0, 12(%eax)
+    # ... and print it!
+    pushl %eax
+    call um_puts
+    addl $4, %esp
+
+    # exit with success!
+    movl $0, %eax # status code 0 (success!)
+
+  4:
+
+    # restore %ebx
+    popl %ebx
 
     leave
     ret
