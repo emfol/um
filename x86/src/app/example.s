648a88417b3fa9da3823c252f30211e8c84cce85
Emanuel F. Oliveira <emfolg@gmail.com>
2016-08-03 00:18:33 -0300

    app: example.s get %eip value...

--- x86/src/app/example.s
+++ x86/src/app/example.s
@@ -12,6 +12,15 @@ main:
     pushl %ebp
     movl %esp, %ebp
 
+    # load %eax with %eip
+    .byte 0xE8, 0x00, 0x00, 0x00, 0x00
+    .byte 0x58 # %eax has the address of this instruction...
+
+    # load %ecx with %eip
+    call 1f
+  1:
+    popl %ecx # %eax has the address of this instruction...
+
     # these bytes will produce the same code below...
     .byte 0xB8, 0x00, 0x00, 0x00, 0x80
     .byte 0x0F, 0xA2
