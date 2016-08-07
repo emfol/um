097f6b889c4ad76d641800e396daf1c94c9dffae
Emanuel F. Oliveira <emfolg@gmail.com>
2016-08-07 18:29:16 -0300

    [WIP] added um_itoa

--- x86/src/lib/um/string.s
+++ x86/src/lib/um/string.s
@@ -18,3 +18,25 @@ um_strlen:
     subl %ecx, %eax
     ret
 
+
+# char *um_itoa( int num, char *buf, int base );
+um_itoa:
+
+    pushl %ebp
+    movl %esp, %ebp
+
+    movl 16(%ebp), %eax
+    cmpl $2, %eax
+    jl 1f
+    cmpl $36, %eax
+    jle 2f
+  1:
+    movl $0, %eax
+    jmp 3f
+
+  2:
+    movl 12(%ebp), %eax
+
+  3:
+    leave
+    ret
