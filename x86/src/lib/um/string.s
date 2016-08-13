cb453ea5933fca98b4e4c9245dc56008c6a53a71
Emanuel F. Oliveira <emfolg@gmail.com>
2016-08-13 20:22:07 -0300

    [WIP] um_itoa -- partial implementation of algorithm for powers of 2

--- x86/src/lib/um/string.s
+++ x86/src/lib/um/string.s
@@ -55,24 +55,63 @@ um_itoa:
     pushl %ebp
     movl %esp, %ebp
 
+    # check value for base...
     movl 16(%ebp), %eax
     cmpl $2, %eax
-    jl 1f
+    jl 1f # invalid base...
     cmpl $36, %eax
-    jle 2f
+    jle 2f # valid base...
+    # ... else, invalid base
+
   1:
     # invalid base...
-    # ... terminate string and return -1.
+    # ... terminate string and return -1
     movl 12(%ebp), %eax
     movb $0, (%eax)
     movl $-1, %eax
-    jmp 3f
+    jmp EXIT
 
   2:
+    # valid base...
+    # ... check if base is a power of 2
+    bsfl %eax, %ecx
+    bsrl %eax, %edx
+    cmpl %ecx, %edx
+    je POW2
+    jne NON_POW2
+
+  POW2:
+    pushl %ebx
+    movl 12(%ebp), %ebx
+    movl 8(%ebp), %edx
+    movb %cl, %ch
+    decb %ch # bitmask ( bitindex - 1 )
+    xorl %eax, %eax
+  POW2_LOOP:
+    movb %ch, %al
+    andl %edx, %eax
+    cmpb $10, %al
+    jl 2f
+    addb $'A', %al
+    jmp 3f
+  2:
+    addb $'0', %al
+  3:
+    movb %al, (%ebx)
+    incl %ebx
+    cmpl $0, %edx
+    je 2f
+    shrl %cl, %edx
+    jmp POW2_LOOP
+  2:
+    movb $0, (%ebx)
+
+
+  NON_POW2:
     movl 12(%ebp), %eax
     movb $0, (%eax)
     movl $0, %eax
 
-  3:
+  EXIT:
     leave
     ret
